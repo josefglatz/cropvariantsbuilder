177df00ccc984b5a0a8f1d24779976fde826b5d3
Josef Glatz <josefglatz@gmail.com>
2020-01-08 12:01:02 +0100

    [TASK] Add Declaration File

---
+++ ext_emconf.php
@@ -0,0 +1,19 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Crop Variants Builder',
+    'description' => 'Simplify writing cropVariants for TYPO3 Integrators/Developers',
+    'version' => '1.0.0',
+    'state' => 'stable',
+    'author' => 'Josef Glatz',
+    'author_email' => 'josefglatz@gmailcom',
+    'clearCacheOnLoad' => true,
+    'category' => 'be',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '8.7.13-9.5.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
