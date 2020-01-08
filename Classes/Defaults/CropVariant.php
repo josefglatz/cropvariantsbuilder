986aa35e7189aff2e32dc074b1f8e805ddaf8a9a
Josef Glatz <josefglatz@gmail.com>
2020-01-08 11:16:50 +0100

    [TASK] Move Classes to extension
    This patch moves the core of the functionality to ext:cropvariantsbuilder

---
+++ Classes/Defaults/CropVariant.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types = 1);
+
+namespace JosefGlatz\CropVariantsBuilder\Backend\CropVariants\Defaults;
+
+class CropVariant
+{
+    /**
+     * Retrieve as default declared cropVariants
+     *
+     * @return array default cropVariants (only key)
+     */
+    public static function getDefaultCropVariantsNames(): array
+    {
+        $defaultCropVariantsConfiguration = Configuration::defaultConfiguration('defaultCropVariantsConfiguration');
+        return array_keys($defaultCropVariantsConfiguration);
+    }
+}
---
+++ Classes/Defaults/FocusArea.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types = 1);
+
+namespace JosefGlatz\CropVariantsBuilder\Backend\CropVariants\Defaults;
+
+class FocusArea
+{
+    /**
+     * Retrieve focus area
+     *
+     * @param string $name
+     * @return array focusArea (no default is returned as there is no focusArea necessary)
+     * @throws \UnexpectedValueException
+     */
+    public static function get(string $name): array
+    {
+        $focusAreas = Configuration::defaultConfiguration('focusAreas');
+        if (isset($focusAreas[$name]) && \is_array($focusAreas[$name])) {
+            return $focusAreas[$name];
+        }
+        throw new \UnexpectedValueException('Given focus area "' . $name . '" not found."', 1522992127);
+    }
+}
