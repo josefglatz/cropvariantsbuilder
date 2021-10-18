6c6bee317211b7b2328ecf392235d1b649f07db3
Ingo Fabbri <inf@mmcagentur.at>
2021-10-18 11:45:15 +0200

    [TASK] make compatible with TYPO3 11 LTS

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'be',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.13-10.4.99',
+            'typo3' => '8.7.13-11.5.99',
             'php' => '7.2.0-0.0.0'
         ],
         'conflicts' => [],
