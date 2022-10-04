c4ed7058aec6ffd906d9abca793ea2a002845f8f
Helmut Strasser <h.strasser@supseven.at>
2022-10-04 11:25:08 +0200

    [BUGFIX] Remove superfluous keys for crop variants.
    The Method Builder->addCropVariant() adds the whole configuration array from the CropVariants.yaml to the final crop variant, which might brake the whole crop functionality in the BE, if coverAreas and focusArea are configured, too. This bugfix removes those superfluous keys/arrays before the configuration is assigned to the crop variant.

--- Classes/Builder.php
+++ Classes/Builder.php
@@ -105,6 +105,11 @@ class Builder
                     1520906581
                 );
             }
+
+            // $item might comprise arrays of coverAreas and focusArea, too. Those arrays must not be assigned
+            // to the cropVariants, otherwise the crop functionality won't work at all.
+            unset($item['coverAreas'], $item['focusArea']);
+
             $this->cropVariants[$key] = $item;
         }
 
