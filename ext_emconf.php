cd6077f73ce2f10fb222b6578252f74fa02cfcbc
Josef Glatz <typo3@josefglatz.at>
2024-10-31 05:35:37 +0100

    [TASK] Raise TYPO3 version contraint
    Related: #30

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,7 +10,7 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'be',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-12.4.99'
+            'typo3' => '11.5.0-13.4.99'
         ],
         'conflicts' => [],
         'suggests' => [],
